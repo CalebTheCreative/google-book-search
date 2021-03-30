810cb9be0abaf95519c0b46af946d5ba0be454f8
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-30 16:43:23 -0500

    Added API for google search.

---
+++ client/src/utils/API.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+export default {
+    
+    // Get google books from search
+    searchGoogle: function(query) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+    },
+    
+    // Get all books
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+
+    // Gets book w/ given id
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+
+    // Deletes book w/ given id
+    deleteBook: function(id) {
+        return axios.delete("/api/books" + id);
+    },
+
+    // Saves book to database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    }
+
+}
