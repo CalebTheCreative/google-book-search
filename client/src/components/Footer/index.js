3491eb1aad4cbd2006a496f1e11c9621dd5efecb
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-30 16:15:30 -0500

    Added footer for page.

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <div className="footer justify-content-center">
+            <p>Copyright 2021 Caleb Hopkins</p>
+        </div>
+    )
+}
+
+export default Footer;
