e9c252b88c791d26a21c1e2411824c040fd42558
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-30 16:13:53 -0500

    Create-react-app #2

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GoogleNav from "./components/GoogleNav"
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <GoogleNav />
+        <Switch>
+          <Route exact path={["/"]}>
+            <Search />
+          </Route>
+          <Route exact path={["/saved", "/saved/:id"]}>
+            <Saved />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
